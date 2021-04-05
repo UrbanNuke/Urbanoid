1b34a2d0d8c73a2937d35b09969860b6ad88b72c
A.Urbanyk <urbanyk@gmail.com>
2021-04-05 19:47:05 +0300

    Add openGL simple debbuger

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -3,6 +3,23 @@
 #include <GLFW/glfw3.h>
 #include "shaderCreator.h"
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define glCall(x) glClearError();\
+	x;\
+	ASSERT(glLogCall(#x, __FILE__, __LINE__));\
+
+static void glClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool glLogCall(const char* fn, const char* file, const int line) {
+	while (const GLenum error = glGetError()) {
+        std::cout << "[OpenGL Error] (" << error << "): " << fn << " " << file << ":" << line << std::endl;
+        return false;
+	}
+    return true;
+}
+
 int main(void) {
     GLFWwindow* window;
 
@@ -11,7 +28,7 @@ int main(void) {
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Arkanoid (c) Urbannuke", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -39,44 +56,45 @@ int main(void) {
         2, 3, 0
     };
 
-    unsigned int vao; // prepare buffer (vertex array object)
-    glGenBuffers(1, &vao); // generate new buffer with size = 1
-    glBindBuffer(GL_ARRAY_BUFFER, vao); // bind our buffer to STATE MACHINE GL_ARRAY_BUFFER = for vertex attributes
-	
-    /*
-     * GL_ARRAY_BUFFER - which the buffer object is bound, 6 - count of values
-     * positions - pointer to our dataArr, GL_STATIC_DRAW - how we should use that data (how a buffer object's data store will be accessed.)
-     */
-	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0); // enable vertex attributes Array
-	
+    unsigned int buffer; // prepare buffer (vertex array object)
+    glCall(glGenBuffers(1, &buffer)); // generate new buffer with size = 1
+    glCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); // bind our buffer to STATE MACHINE GL_ARRAY_BUFFER = for vertex attributes
+
+     /*
+      * GL_ARRAY_BUFFER - which the buffer object is bound, 6 - count of values
+      * positions - pointer to our dataArr, GL_STATIC_DRAW - how we should use that data (how a buffer object's data store will be accessed.
+      */
+    glCall(glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW));
+    glCall(glEnableVertexAttribArray(0)); // enable vertex attributes Array
+
     /**
      *  0 - position, 2 - size (1 vert = 2 floats), GL_FLOAT - what kind of data we use,
      *  GL_FALSE - normalization, stride - how many bites takes one vert, pointer - begining of our first vert
      */
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    glCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
     unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    glCall(glGenBuffers(1, &ibo));
+    glCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    glCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
     const unsigned int shader = createShader("res/shaders/main.shader");
-    glUseProgram(shader);
+    glCall(glUseProgram(shader));
 
     const int location = glGetUniformLocation(shader, "u_Color");
-    glUniform4f(location, 0.8f, 0.5f, 0.2f, 1.0f);
+    glCall(glUniform4f(location, 0.8f, 0.5f, 0.2f, 1.0f));
 
     float increment = 0.05f;
     float r = 0.0f;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        glUniform4f(location, r, 0.5f, 0.2f, 1.0f);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        /* Render here */
+        glCall(glClear(GL_COLOR_BUFFER_BIT));
+
+        glCall(glUniform4f(location, r, 0.5f, 0.2f, 1.0f));
+        glCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         if (r > 1.0f) {
             increment = -0.05f;
@@ -87,13 +105,13 @@ int main(void) {
         r += increment;
     	
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glCall(glfwSwapBuffers(window));
 
         /* Poll for and process events */
-        glfwPollEvents();
+        glCall(glfwPollEvents());
     }
 
-    glDeleteProgram(shader);
+    glCall(glDeleteProgram(shader));
 
     glfwTerminate();
     return 0;
