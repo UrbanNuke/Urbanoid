9ed6998e293cd6638a7a689b75cdec8a55bd05e7
A.Urbanyk <urbanyk@gmail.com>
2021-04-05 18:09:35 +0300

    Add index buffer

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -25,35 +25,39 @@ int main(void) {
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float positions[] = {
+        -0.5f, -0.5f, // 0
+         0.5f, -0.5f, // 1
+         0.5f,  0.5f, // 2
+    	-0.5f,  0.5f, // 3
     };
 
-    GLuint buffer; // prepare buffer
-    glGenBuffers(1, &buffer); // generate new buffer with size = 1
-    glBindBuffer(GL_ARRAY_BUFFER, buffer); // bind our buffer to STATE MACHINE GL_ARRAY_BUFFER = for vertex attributes
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    unsigned int vao; // prepare buffer (vertex array object)
+    glGenBuffers(1, &vao); // generate new buffer with size = 1
+    glBindBuffer(GL_ARRAY_BUFFER, vao); // bind our buffer to STATE MACHINE GL_ARRAY_BUFFER = for vertex attributes
 	
     /*
-     * GL_ARRAY_BUFFER - which the buffer object is bound
-     * 6 - count of values
-     * positions - pointer to our dataArr
-     * GL_STATIC_DRAW - how we should use that data (how a buffer object's data store will be accessed.)
+     * GL_ARRAY_BUFFER - which the buffer object is bound, 6 - count of values
+     * positions - pointer to our dataArr, GL_STATIC_DRAW - how we should use that data (how a buffer object's data store will be accessed.)
      */
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-
+	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0); // enable vertex attributes Array
 	
     /**
-     *  0 - position,
-     *  2 - size (1 vert = 2 floats),
-     *  GL_FLOAT - what kind of data we use,
-     *  GL_FALSE - normalization,
-     *  stride - how many bites takes one vert
-     *  pointer - begining of our first vert
+     *  0 - position, 2 - size (1 vert = 2 floats), GL_FLOAT - what kind of data we use,
+     *  GL_FALSE - normalization, stride - how many bites takes one vert, pointer - begining of our first vert
      */
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     const unsigned int shader = createShader("res/shaders/main.shader");
     glUseProgram(shader);
@@ -63,8 +67,8 @@ int main(void) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    	
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
