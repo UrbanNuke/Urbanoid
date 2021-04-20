c879db605704ebb850b849ddda1a01ad9d4bacc6
A.Urbanyk <turbino4ka91@gmail.com>
2021-04-20 20:39:08 +0300

    fix: vertexBufferLayout template

--- App/src/openGL/vertexBufferLayout.cpp
+++ App/src/openGL/vertexBufferLayout.cpp
@@ -1,6 +1,4 @@
 #include "vertexBufferLayout.h"
-#include <GL/glew.h>
-#include "renderer.h"
 
 unsigned int VertexBufferObjElement::getSizeOfType(const unsigned int type) {
 	switch (type) {
@@ -13,27 +11,4 @@ unsigned int VertexBufferObjElement::getSizeOfType(const unsigned int type) {
 		ASSERT(false)
 			return 0;
 	}
-}
-
-template <typename T>
-void VertexBufferLayout::push(unsigned int count) {
-	static_assert(false);
-}
-
-template <>
-void VertexBufferLayout::push<float>(const unsigned int count) {
-	m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
-	m_Stride += count * VertexBufferObjElement::getSizeOfType(GL_FLOAT);
-}
-
-template <>
-void VertexBufferLayout::push<unsigned int>(const unsigned int count) {
-	m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
-	m_Stride += count * VertexBufferObjElement::getSizeOfType(GL_UNSIGNED_INT);
-}
-
-template <>
-void VertexBufferLayout::push<unsigned char>(const unsigned int count) {
-	m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
-	m_Stride += count * VertexBufferObjElement::getSizeOfType(GL_UNSIGNED_BYTE);
 }
